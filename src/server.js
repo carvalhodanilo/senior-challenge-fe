3b667bd80f5e3f94fff2bc13ace7f71c71435192
jimyds <danilocarvalho.ep@gmail.com>
2021-07-28 23:11:21 -0300

    chore: server.js

--- src/server.js
+++ src/server.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/danilo-challenge-senior-fe'));
+app.use(express.static(`${__dirname}/dist/`));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/danilo-challenge-senior-fe/'}),
-);
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/index.html`));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
