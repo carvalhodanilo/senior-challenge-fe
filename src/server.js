9d66dd0a172c13bc8e1ea8eb6c61edc279e76866
jimyds <danilocarvalho.ep@gmail.com>
2021-07-28 23:39:10 -0300

    chore: server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
